ba22fc91391479b26d47a5c128ad49a5813693a3
danilopolani <danilo.polani@gmail.com>
2022-08-20 15:35:29 +0200

    expand test

--- tests/Unit/Template/RenderTest.php
+++ tests/Unit/Template/RenderTest.php
@@ -14,21 +14,21 @@ it('transforms plain variables', function () {
 
 it('transforms direct access to array properties', function () {
     $result = invade(Render::resolve())->parse(<<<TEMPLATE
-        {{foo['bar']}} {{\$bar['baz']}} {{foo['bar']['baz']}} {{bar['baz']}} {{bar[0]}}
+        {{foo['bar']}} {{\$bar['baz']}} {{foo['bar']['baz']}} {{bar['baz']}} {{bar[0]}} {{bar[0][1]}}
     TEMPLATE);
 
     expect($result)->toBe(<<<TEMPLATE
-        {\$foo['bar']} {\$bar['baz']} {\$foo['bar']['baz']} {\$bar['baz']} {\$bar[0]}
+        {\$foo['bar']} {\$bar['baz']} {\$foo['bar']['baz']} {\$bar['baz']} {\$bar[0]} {\$bar[0][1]}
     TEMPLATE);
 });
 
 it('transforms dot notation access to array properties', function () {
     $result = invade(Render::resolve())->parse(<<<TEMPLATE
-        {{foo.bar}} {{\$bar.baz}} {{foo['bar']['baz']}} {{bar.baz}} {{foo[0].bar}} {{foo.0.bar.baz}}
+        {{foo.bar}} {{\$bar.baz}} {{foo['bar']['baz']}} {{bar.baz}} {{foo[0].bar}} {{foo.0.bar.baz}} {{foo.0.1.bar.2.baz}}
     TEMPLATE);
 
     expect($result)->toBe(<<<TEMPLATE
-        {\$foo['bar']} {\$bar['baz']} {\$foo['bar']['baz']} {\$bar['baz']} {\$foo[0]['bar']} {\$foo[0]['bar']['baz']}
+        {\$foo['bar']} {\$bar['baz']} {\$foo['bar']['baz']} {\$bar['baz']} {\$foo[0]['bar']} {\$foo[0]['bar']['baz']} {\$foo[0][1]['bar'][2]['baz']}
     TEMPLATE);
 });
 

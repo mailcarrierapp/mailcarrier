df4e47ccd167ecc43b1cc763bf9edd65c9f707e8
danilopolani <danilo.polani@gmail.com>
2022-08-24 22:42:04 +0200

    fix help with syntax url

--- src/Resources/LayoutResource.php
+++ src/Resources/LayoutResource.php
@@ -102,7 +102,7 @@ class LayoutResource extends Resource
 
                 MonacoEditor::make('content')
                     ->required()
-                    ->hint('<a href="https://twig.symfony.com/doc/3.x/templates.html" class="underline text-primary-500 cursor-help" target="_blank" tabindex="-1">Help with template Syntax</a>')
+                    ->hint(new HtmlString('<a href="https://twig.symfony.com/doc/3.x/templates.html" class="underline text-primary-500 cursor-help" target="_blank" tabindex="-1">Help with syntax</a>'))
                     ->hintIcon('heroicon-o-code')
                     // Full width
                     ->columnSpan(2)

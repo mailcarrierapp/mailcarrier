c075e04314b673e552faf7132fd93397a9a9c1e9
danilopolani <danilo.polani@gmail.com>
2024-03-17 12:37:05 +0100

    fix linting

--- database/migrations/1_create_users_table.php
+++ database/migrations/1_create_users_table.php
@@ -22,7 +22,6 @@ return new class extends Migration {
             $table->timestamps();
         });
 
-
         if (!Schema::hasTable('password_reset_tokens')) {
             Schema::create('password_reset_tokens', function (Blueprint $table) {
                 $table->string('email')->primary();

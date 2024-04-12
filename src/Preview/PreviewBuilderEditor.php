8b804056899e93bf26a57a759b30de07fc9e4497
danilopolani <danilo.polani@gmail.com>
2024-04-12 15:00:40 +0200

    override builder editor view

--- src/Preview/PreviewBuilderEditor.php
+++ src/Preview/PreviewBuilderEditor.php
@@ -2,9 +2,21 @@
 
 namespace MailCarrier\Preview;
 
+use Illuminate\Contracts\View\View as ViewContract;
 use Pboivin\FilamentPeek\Livewire\BuilderEditor;
 
 class PreviewBuilderEditor extends BuilderEditor
 {
-    //
+    public function render(): ViewContract
+    {
+        if ($this->shouldAutoRefresh()) {
+            try {
+                $this->refreshBuilderPreview();
+            } catch (ValidationException $e) {
+                // pass
+            }
+        }
+
+        return view('mailcarrier::livewire.preview.builder-editor');
+    }
 }

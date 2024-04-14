9e4c7c0c0f48ba0bffbc63d111df925e31269b18
danilopolani <danilo.polani@gmail.com>
2024-04-14 12:40:31 +0200

    show notification when accepting live preview edits

--- src/Preview/PreviewBuilderEditor.php
+++ src/Preview/PreviewBuilderEditor.php
@@ -2,6 +2,7 @@
 
 namespace MailCarrier\Preview;
 
+use Filament\Notifications\Notification;
 use Illuminate\Contracts\View\View as ViewContract;
 use Pboivin\FilamentPeek\Livewire\BuilderEditor;
 
@@ -18,5 +19,15 @@ class PreviewBuilderEditor extends BuilderEditor
         }
 
         return view('mailcarrier::livewire.preview.builder-editor');
+    }
+
+    public function closeBuilderEditor(): void
+    {
+        parent::closeBuilderEditor();
+
+        Notification::make()
+            ->title('Remember to save your changes!')
+            ->info()
+            ->send();
     }
 }

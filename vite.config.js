d76c6f0d067e6847bdcb7b4517c31a4d67e51930
danilopolani <danilo.polani@gmail.com>
2024-04-16 10:11:07 +0200

    replace mix with vite

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  build: {
+    outDir: 'resources/dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name].js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: 'css/[name].[ext]',
+      }
+    }
+  },
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/theme.css',
+        'resources/js/codemirror.component.js',
+      ],
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'resources/images',
+          dest: ''
+        },
+      ],
+    }),
+  ],
+});

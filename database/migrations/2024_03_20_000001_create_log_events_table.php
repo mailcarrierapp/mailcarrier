9b5f000082e3d4e026dcd5078666f1c1385f2c39
danilopolani <danilo.polani@gmail.com>
2025-05-21 17:49:37 +0200

    add log events table

---
+++ database/migrations/2024_03_20_000001_create_log_events_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('log_events', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('log_id')->index()->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('log_events');
+    }
+};

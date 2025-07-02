1ed159601c1875956fb1ee2f7d10ea47d2f6d648
danilopolani <danilo.polani@gmail.com>
2025-07-02 14:51:27 +0200

    fix(webhooks): use correct messageId from MailGun

--- src/Webhooks/Strategies/MailgunStrategy.php
+++ src/Webhooks/Strategies/MailgunStrategy.php
@@ -3,6 +3,7 @@
 namespace MailCarrier\Webhooks\Strategies;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Config;
 use MailCarrier\Webhooks\Dto\IncomingWebhook;
 use MailCarrier\Webhooks\Dto\WebhookData;
@@ -75,13 +76,14 @@ class MailgunStrategy implements Strategy
         }
 
         $eventData = $payload['event-data'];
+        $messageId = Arr::get($eventData, 'message.headers.message-id');
 
-        if (!isset($eventData['id'], $eventData['event'], $eventData['timestamp'])) {
+        if (!isset($messageId, $eventData['event'], $eventData['timestamp'])) {
             throw new \InvalidArgumentException('Invalid Mailgun webhook payload: missing required fields');
         }
 
         return new WebhookData(
-            messageId: $eventData['id'],
+            messageId: $messageId,
             eventName: $eventData['event'],
             date: CarbonImmutable::createFromTimestamp($eventData['timestamp'])
         );
